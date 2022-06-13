40aa23fc28180167dd328b4410a88ba28326760b
Abdus-Samad Charles <a.charles.edu@gmail.com>
2022-06-13 20:54:52 +0200

    updated fetch3

--- fetchexamples/fetch3/script/script.js
+++ fetchexamples/fetch3/script/script.js
@@ -13,35 +13,31 @@ async function displayData() {
     // console.log(
     // `${item['name'].title} - ${item['name'].first}`);
     let result = `
-        <div class="card m-3 p-3" style="width:400px">
+        <div class="card m-3 p-3 text-center" style="width:400px">
             <img class="rounded-circle w-50 mx-auto" src="${item["picture"].large}" alt="Card image">
             <div class="card-body">
-                <h4 id="test" class="card-title">${item["name"].first} - ${item["name"].last}</h4>
-                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                <a href="#" class="btn btn-primary">See Profile</a>
-
-                <p id="user_title"></p>
-                <p id="user_value"></p>
+            <p class="card-text">My name is</p>
+            <h4 id="test" class="card-title">${item["name"].first} ${item["name"].last}</h4>
 
                 <ul class="values_list horizontal_center" id="values_list">
-                    <li data-title="Hi, My name is" data-value="Aiden Carpenter" data-label="name" class="">
-                    <i onmouseover="show('my name is','${item['name'].first} ${item['name'].last}')" class="fa-solid fa-circle-user"></i>
+                    <li>
+                    <i onmouseover="show('My name is','${item['name'].first} ${item['name'].last}')" class="fa-solid fa-circle-user"></i>
                     </li>
                 
-                    <li data-title="My email address is" data-value="aiden.carpenter@example.com" data-label="email" data-caps="false" class="">
-                    <i onmouseover="show('my name is','${item['name'].first} ${item['name'].last}')" class="fa-solid fa-envelope"></i>
+                    <li>
+                    <i onmouseover="show('My Email is','${item['email']}')" class="fa-solid fa-envelope"></i>
                     </li>
                 
-                    <li data-title="My birthday is" data-value="6/7/1975" data-label="birthday" class="">
-                    <i onmouseover="show('my name is','${item['name'].first} ${item['name'].last}')" class="fa-solid fa-calendar-days"></i>
+                    <li>
+                    <i onmouseover="show('My Birthday is','${item['dob'].date}')" class="fa-solid fa-calendar-days"></i>
                     </li>
                 
-                    <li data-title="My address is" data-value="2590 Lakeshore Rd" data-label="location" class="">
-                    <i onmouseover="show('my name is','${item['name'].first} ${item['name'].last}')" class="fa-solid fa-map-location-dot"></i>
+                    <li>
+                    <i onmouseover="show('My address is','${item['location']['street'].number} ${item['location']['street'].name} ${item['location'].city}')" class="fa-solid fa-map-location-dot"></i>
                     </li>
                     
-                    <li data-title="My phone number is" data-value="(927)-208-3044" data-label="phone" class="">
-                    <i onmouseover="show('my name is','${item['name'].first} ${item['name'].last}')" class="fa-solid fa-phone"></i>
+                    <li>
+                    <i onmouseover="show('My Phone Number is','${item['cell']}')" class="fa-solid fa-phone"></i>
                     </li>
                 </ul>
             </div>
